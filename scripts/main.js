8d9a9f96cb9ae96b795090059070ddfcd2112ca1
Madison Togrul <mtogrul@umich.edu>
2019-04-09 17:31:12 -0700

    update with js

--- scripts/main.js
+++ scripts/main.js
@@ -15,3 +15,28 @@ function divide(num1,num2) {
   return answer;
 }
 document.getElementById("answer").innerHTML=divide(20, 5);
+var myImage = document.querySelector('img');
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/happy-squirrel.jpg') {
+      myImage.setAttribute ('src','images/madi.jpg');
+    } else {
+      myImage.setAttribute ('src','images/happy-squirrel.jpg');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Hey there, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Hey there, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
